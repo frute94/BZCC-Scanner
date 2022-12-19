c39794a024d646406a0344681280974d4b0dc3f6
frute94 <frutesoftware@gmail.com>
2022-12-19 00:25:41 -0600

    Delete bzccb64.h

--- bzccb64.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int _b64decode(const char *b64, char *output, int output_size);
-
-// Writes BAD_B64 to output on failure
-#define BAD_B64 "B64 DECODING ERROR"
-int b64decode(const char *b64, char *output, int output_size);
